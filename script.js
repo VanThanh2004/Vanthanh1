17ba80284777ce153d102eb1ed9dd8b2c94aa9a2
Văn Thành <161943245+VanThanh2004@users.noreply.github.com>
2026-07-07 14:35:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fileInput = document.getElementById('fileInput');
 const gallery = document.getElementById('gallery');
 const count = document.getElementById('count');
 
-const API_KEY = "6xbtkHv7N4JJENoVhAHedQEm";
+const API_KEY = "2rtYbtA3CfCiHVSJPiBAgAt5";
 
 let totalImages = 0;
 const MAX_IMAGES = 250;
